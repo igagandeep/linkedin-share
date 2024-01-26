cc4c995a30a3fded76126da69487c61e53a47556
Gagandeep <gagan@prepr.org>
2024-01-26 13:00:28 -0500

    commit 10

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import Share from './components/Share';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Link,
 } from 'react-router-dom';
+import React, { lazy, Suspense } from 'react';
+
+
+const Share = lazy(() => import('./components/Share'));
 
 function App() {
   return (
@@ -12,7 +15,14 @@ function App() {
       <h1>Hello React World</h1>
       <Link to="/share/12345">Share</Link>
       <Routes>
-        <Route path="/share/12345" element={<Share />} />
+      <Route
+          path="/share/12345"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Share />
+            </Suspense>
+          }
+        />
       </Routes>
     </Router>
   )
